77bf1b938f3250fd7b192de9ecde51dd780af6fb
Anix Sam Saji <70740206+anixsam@users.noreply.github.com>
2022-07-08 12:28:02 +0530

    new file:   README.md 	new file:   external.txt 	new file:   output.txt 	new file:   script.py

---
+++ README.md
@@ -0,0 +1,4 @@
+# Program to create shortcut when an external storage device is plugged in
+
+## Step 1 : Install Python
+## Step2 : Copy the set-shortcut file to system32 folder
---
+++ script.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+system_drives = ['C','D']
+
+def run(cmd):
+    subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+
+while True:
+    try:
+        os.system('wmic logicaldisk get caption > output.txt')
+        drives = open("output.txt","r")
+        drives_list = drives.readlines()
+        drives.close()
+        drives_list.pop(0)
+        external_drives = []
+        for x in drives_list:
+            output = x.rsplit('\x00')
+            if len(output) != 2 and output[1] not in system_drives :
+                external_drives.append(output[1])
+        old_extern = open("external.txt","r")
+        old_external_drives = old_extern.read()
+        old_extern.close()
+
+
+        extern = open("external.txt","w")
+
+        for x in old_external_drives:
+            if x not in external_drives:
+                run("del C:/users/anixs/Desktop/"+x+".lnk") 
+
+        for x in external_drives:
+            if x not in old_external_drives:
+                run("set-shortcut C:/users/anixs/Desktop/" + x + ".lnk " + x + ":/")
+            extern.write(x)
+
+        extern.close()
+
+    # extern = open("external.txt","w")
+    # for x in external_drives:
+    #     run("set-shortcut C:/users/anixs/Desktop/" + x + ".lnk " + x + ":/")
+    #     extern.write(x)  
+    # extern.close()
+
+    except:
+        print("Error")
+
+    
